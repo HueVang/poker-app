a1c7039f5d843322f4d2aa8d064f2bc2ce9f3bac
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-03-01 16:45:37 -0600

    Added admin and player folder with js files in routes folder. Also added the routes in the server js file.

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ var passport = require('passport');
 var connection = require('./db/connection');
 var login = require('./routes/login');
 var register = require('./routes/register');
+var adminInterface = require('./routes/admin/admin.interface.js');
+var createLeague = require('./routes/admin/create.league.js');
+var currentGame = require('./routes/admin/current.game.js');
+var leaderBoard = require('./routes/admin/leader.board.js');
+var playerRoster = require('./routes/admin/player.roster.js');
+var scheduleNewGame = require('./routes/admin/schedule.new.game.js');
+// var createPlayerProfile = require('./routes/player/create.player.profile.js'); Already included in register route
+var editPlayerProfile = require('./routes/player/edit.player.profile.js');
+var otherPlayerProfile = require('./routes/player/other.player.profile.js');
+var playerInterface = require('./routes/player/player.interface.js');
 
 require('./auth/setup');
 
@@ -37,6 +47,19 @@ app.use('/register', register);
 app.get('/loginStatus', function(req, res){
   res.send(req.isAuthenticated());
 });
+app.use('/adminHome' ,adminInterface);
+app.use('/league' ,createLeague);
+app.use('/game' ,currentGame);
+app.use('/leaderboard' ,leaderBoard);
+app.use('/roster' ,playerRoster);
+app.use('/newGame' ,scheduleNewGame);
+// app.use('/newprofile' ,createPlayerProfile); Already included in register route
+app.use('/editProfile' ,editPlayerProfile);
+app.use('/otherProfile' ,otherPlayerProfile);
+app.use('/playerHome' ,playerInterface);
+
+
+
 
 // the following routes require authentication
 app.use('/private', ensureAuthenticated);
