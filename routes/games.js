d557528d5de2a3acf315fe23494013ceb42f33a4
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2017-03-03 11:36:15 -0600

    Added more server-side routes

--- routes/games.js
+++ routes/games.js
@@ -27,5 +27,49 @@ router.post('/', function(req, res){
   });
 });
 
+router.get('/games', function(req, res){
+  pool.connect(function(err, client, done){
+    if(err){
+      console.log('Error connecting to the DB', err);
+      res.sendStatus(500);
+      done();
+    } else {
+      client.query('SELECT * FROM games WHERE league_id = $1',
+      [req.body.league_id], function(err, result){
+        done();
+        if (err){
+          console.log('Error querying DB', err);
+          res.sendStatus(500);
+          }else{
+            console.log('Got info from DB', result.rows)
+            res.send(result.rows);
+          }
+        });
+    }
+  });
+});
+
+router.put('/:id', function(req, res){
+ pool.connect(function(err, client, done){
+   if (err) {
+     console.log('Error connecting to DB', err);
+     res.sendStatus(500);
+     done();
+   } else {
+     client.query('UPDATE games SET name=$2, date=$3, time=$4 WHERE id = $1 RETURNING *',
+        [req.params.id, req.body.name, req.body.date, req.body.time],
+        function(err, result){
+          done();
+        if (err) {
+          console.log('Error updating users', err);
+          res.sendStatus(500);
+        } else {
+          res.send(result.rows);
+        }
+      });
+   }
+ });
+});
+
 
 module.exports = router;
