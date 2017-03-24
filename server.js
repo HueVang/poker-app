2fe3aa641b4c22f8d32013bb0d9f1fb9fdf4bfd7
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-03-24 12:46:02 -0500

    Added process.env.PORT to server.js file

--- server.js
+++ server.js
@@ -92,6 +92,6 @@ io.on('connection', function(socket){
 
 });
 
-var server = http.listen(3000, function() {
-  console.log('Listening on port', 3000);
+var server = http.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port');
 });
